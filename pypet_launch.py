ba34fe148ef88937855f20ab8daaf88c5c7ee89e
Raphael Holca <raphaelholca@gmail.com>
2017-01-27 17:08:40 +0100

    parameter update

--- pypet_launch.py
+++ pypet_launch.py
@@ -32,7 +32,7 @@ parameter_dict = {	'conv_dHigh'			: 2.7,
 					'feedf_dMid' 			: 0.02,
 					'feedf_dNeut' 			: 0.01, 
 					'feedf_dLow' 			: -2.0,
-					'name' 					: 'pypet_conv_xplr',
+					'name' 					: 'pypet_feedf_greedy_3',
 					'n_epi_crit' 			: 0,
 					'n_epi_dopa' 			: 6,
 					'A' 					: 900.,
@@ -43,8 +43,8 @@ parameter_dict = {	'conv_dHigh'			: 2.7,
 					'conv_map_num' 			: 20,
 					'conv_filter_side'		: 5,
 					'feedf_neuron_num'		: 49,
-					'explore_layer'			: 'conv',
-					'dopa_layer'			: 'conv',
+					'explore_layer'			: 'none',
+					'dopa_layer'			: 'feedf',
 					'noise_explore'			: 0.2,
 					'classifier'			: 'neural_prob',
 					'init_file' 			: 'output/pretrain_lr_e-6_t_e-0/pretrain_lr_e-6_t_e-0',
@@ -53,17 +53,17 @@ parameter_dict = {	'conv_dHigh'			: 2.7,
 
 """ explored parameters """
 explore_dict = {	
-					'conv_dHigh'			: [+4.00, +8.00, +12.0, +2.00, +2.50],
-					'conv_dNeut'			: [-0.10, -0.25, -0.75, +2.00, +2.50],
+					# 'conv_dHigh'			: [+2.00, +4.00, +8.00, +12.0],
+					# 'conv_dNeut'			: [-1.00, -0.50, -0.10, -0.01],
 
-					'conv_dMid'			: [-0.01, +0.00, +0.01, +2.00, +2.50],
-					'conv_dLow'			: [-1.00, -2.00, -3.00, +2.00, +2.50]
+					# 'conv_dMid'				: [+0.01, +0.10, +0.50, +1.00],
+					# 'conv_dLow'				: [-2.00]
 
 					# 'feedf_dHigh'			: [+4.00, +6.00, +8.00, +12.0],
 					# 'feedf_dNeut'			: [-0.50, -0.10, -0.05, -0.01],
 					
-					# 'feedf_dMid'			: [+0.01, +0.02, +0.05, +0.10],
-					# 'feedf_dLow'			: [-2.00],
+					'feedf_dMid'			: [+0.00, +0.10, +0.20, +0.30, +0.40, +0.60, +1.00],
+					'feedf_dLow'			: [-2.00, -1.00, -0.50, -0.10, +0.00],
 					
 					# 'conv_dMid'			: [+0.00, +0.10, +0.50, +1.00],
 					# 'conv_dLow'			: [-1.00, -0.70, +0.30, -0.00, +0.30, +0.70, +1.00],
