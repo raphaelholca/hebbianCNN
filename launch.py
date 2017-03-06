63b2723663a99400c84c1b71e57de162f6ff4af5
Raphael Holca <raphaelholca@gmail.com>
2017-03-06 19:06:45 +0100

    creates independent parameters for the exploration parameter for the convolutional and feedforward layers

--- launch.py
+++ launch.py
@@ -27,12 +27,12 @@ parameter_dict = {	'conv_dHigh'			: 2.7,
 					'conv_dNeut' 			: -0.07 ,
 					'conv_dLow' 			: -1.9,
 					'feedf_dHigh'			: 6.0,
-					'feedf_dMid' 			: 0.1, #0.05, 
+					'feedf_dMid' 			: 0.01, #0.05, 
 					'feedf_dNeut' 			: -0.05,
 					'feedf_dLow' 			: -1.0, #-2.0,
-					'name' 					: '7_conv_greedy_feedf_hebb_launch',
-					'n_epi_crit' 			: 6,
-					'n_epi_dopa' 			: 6,
+					'name' 					: 'test_xplor_conv_times3',
+					'n_epi_crit' 			: 0,
+					'n_epi_dopa' 			: 1,
 					'A' 					: 900.,
 					'lr_conv' 				: 1e-6,
 					'lr_feedf' 				: 0.01,
@@ -40,29 +40,30 @@ parameter_dict = {	'conv_dHigh'			: 2.7,
 					't_feedf'				: 1.0,
 					'batch_size' 			: 196,
 					'conv_map_num' 			: 20,
-					'conv_filter_side'		: 5,
+					'conv_filter_side'		: 9,
 					'subs_stride' 			: 2,
-					'feedf_neuron_num'		: 49,
-					'explore_layer'			: 'none',
+					'feedf_neuron_num'		: 16,
+					'explore_layer'			: 'conv',
 					'dopa_layer'			: 'conv',
-					'noise_explore'			: 0.2,
+					'noise_explore_conv'	: 0.8,
+					'noise_explore_feedf'	: 0.2,
 					'classifier'			: 'neural_prob',
-					'init_file' 			: '', #'output/pretrain_lr_e-6_t_e-0_nolearn_conv', #'output/pretrain_lr_e-6_t_e-0'
+					'init_file' 			: 'output/pretrain_fsize_9/pretrain_fsize_9', #'output/pretrain_lr_e-6_t_e-0_nolearn_conv', #'output/pretrain_lr_e-6_t_e-0'
 					'seed' 					: 954,
-					'verbose'		 		: 1
+					'verbose'		 		: 2
 					}
 
 """ load and pre-process training and testing images """
 images_train, labels_train, images_test, labels_test = helper.load_images(	
-																			classes 		= np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], dtype=int),
-																			# classes 		= np.array([4, 7, 9], dtype=int),
+																			# classes 		= np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], dtype=int),
+																			classes 		= np.array([4, 7, 9], dtype=int),
 																			dataset_train	= 'train',
 																			dataset_path 	= '/Users/raphaelholca/Documents/data-sets/MNIST',
 																			pad_size 		= (parameter_dict['conv_filter_side']-1)/2,
 																			load_test 		= True
 																			)
 
-n_runs = 5
+n_runs = 1
 
 run_start = time.time()
 save_path = os.path.join('output', parameter_dict['name'])
